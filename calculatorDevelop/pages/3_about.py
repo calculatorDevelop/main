fef5b5e4269ae4607483bf2ac7a3d3bf530b8b02
twigger123 <113093710+twigger123@users.noreply.github.com>
2023-10-11 22:28:28 +0800

    Add files via upload
    完善了about页

---
+++ calculatorDevelop/pages/3_about.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('more information')
+st.subheader('找到我们')
+st.markdown(' - :tada: 一起[开源](https://github.com/calculatorDevelop)')
+st.markdown('\n')
+st.subheader('了解游戏')
+st.markdown(' - :no_entry_sign: [一键变强](https://bbs.mihoyo.com/ys/strategy/?bbs_presentation_style=no_header) :no_entry_sign: ')
