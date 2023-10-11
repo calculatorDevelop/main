05b27089af9255ab4f6cc6010ec4743eb25e5af8
twigger123 <113093710+twigger123@users.noreply.github.com>
2023-10-11 22:27:02 +0800

    Add files via upload
    完善了welcome页

---
+++ calculatorDevelop/welcome.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.balloons()
+st.title('welcome!')
+st.header('欢迎使用原神计算器！')
+st.markdown('\n')
+st.subheader(' :point_left: 简单快捷的科学计算器')
+st.markdown(''' - **计算硬核数据，钻研硬核内容** :sunglasses:\n
+- **七圣召唤，拒绝口算** :sunglasses:''')
+st.markdown('\n')
+st.subheader(' :point_left: 个性化的伤害计算器')
+st.markdown(''' - **精细计算，伤害一目了然** :sunglasses:\n
+- **精细规划，养成不走弯路** :sunglasses:''')
